5a6bf2aaa6eb64cc7c8e3dd659fbd2dba3a2676d
Muthoni2000 <bmuthoni39@yahoo.com>
2020-04-06 06:48:03 +0300

    adding of quote detail component and @NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TagLineComponent } from './tag-line/tag-line.component';
+
+import { FormsModule } from '@angular/forms';
+import { QuoteDisplayComponent } from './quote-display/quote-display.component';
+import { DateCountPipe } from './date-count.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { HttpClientModule } from '@angular/common/http';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TagLineComponent,
+    QuoteDisplayComponent,
+    DateCountPipe,
+    HighlightDirective,
+    QuoteFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
